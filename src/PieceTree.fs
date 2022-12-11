e5da8152c1e255895ef98158adf74c3e9b590c7a
Humza Shahid <humzasaur@gmail.com>
2022-12-11 19:06:42 +0000

    piece tree progress

--- src/PieceTree.fs
+++ src/PieceTree.fs
@@ -38,6 +38,32 @@ module PieceTree =
                 T(lvx + 1, size left, left, ky, size right, right)
         | t -> t
 
+    let rec private dellrg = function
+        | T(_, _, l, v, _, E) -> (l, v)
+        | T(h, _, l, v, sr, r) ->
+            let (newLeft, newVal) = dellrg l
+            T(h, size newLeft, newLeft, v, sr, r), newVal
+        | _ -> failwith "unexpected dellrg case"
+
+    let private adjust = function
+        | T(lvt, _, lt, kt, _, rt) as t when lvl lt >= lvt - 1 && lvl rt >= (lvt - 1) 
+            -> t
+        | T(lvt, sl, lt, kt, sr, rt) when lvl rt < lvt - 1 && sngl lt-> 
+            skew <| T(lvt - 1, sl, lt, kt, sr, rt)
+        | T(lvt, _, T(lv1, sizeA, a, kl, _, T(lvb, sizeLb, lb, kb, sizeRb, rb)), kt, sizeRt, rt) 
+            when lvl rt < lvt - 1 -> 
+                let left = T(lv1, sizeA, a, kl, sizeLb, lb)
+                let right = T(lvt - 1, sizeRb, rb, kt, sizeRt, rt)
+                T(lvb + 1, size left, left, kb, size right, right)
+        | T(lvt, sl, lt, kt, sr, rt) when lvl rt < lvt -> 
+            split <| T(lvt - 1, sl, lt, kt, sr, rt)
+        | T(lvt, sizeLt, lt, kt, sr1, T(lvr, sl2, T(lva, sizeC, c, ka, sizeD, d), kr, sizeB, b)) -> 
+            let a = T(lva, sizeC, c, ka, sizeD, d)
+            let right = split <| T(nlvl a, sizeD, d, kr, sizeB, b)
+            let left = T(lvt-1, sizeLt, lt, kt, sizeC, c)
+            T(lva + 1, size left, left,  ka, size right, right)
+        | _ -> failwith "unexpected adjust case"
+
     /// O(1): Returns a boolean if tree is empty.
     let isEmpty = function
         | E -> true

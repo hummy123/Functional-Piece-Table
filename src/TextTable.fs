77c594f61c3f14872962f9bc33736839de11dbf7
Humza Shahid <humzasaur@gmail.com>
2022-12-14 08:48:06 +0000

    minor consolidation optimisation

--- src/TextTable.fs
+++ src/TextTable.fs
@@ -21,8 +21,10 @@ module TextTable =
     /// Consolidates a table into a buffer with only used characters and a single piece.
     /// Recommended to call this in another thread: do not use it synchronously.
     let consolidate table =
-        let oldPieces = ListZipper.ofList table.Pieces
-        let buffer = List.fold (fun acc piece -> Buffer.append (Piece.text piece table) acc ) Buffer.empty oldPieces
+        let folder acc piece = 
+            Buffer.append (Piece.text piece table) acc) 
+        let buffer = List.fold folder Buffer.empty table.Pieces.Path
+        let buffer = List.fold folder buffer table.Pieces.Focus
         let piece = Piece.createWithSpan (Span.createWithLength 0 buffer.Length)
         let zipper = {Focus = [piece]; Path = []; Index = 0 }
         {Pieces = zipper; Buffer = buffer;}
